35e8ad189ac0426d7af5da1c16555be6922d24ff
ranisilvani <ranisyach@gmail.com>
2024-07-21 21:53:11 +0700

    Menambahkan endpoint books

--- src/main/java/com/polyglot/training/controller/ControllerHome.java
+++ src/main/java/com/polyglot/training/controller/ControllerHome.java
@@ -1,12 +1,25 @@
 package com.polyglot.training.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ControllerHome {
-    @GetMapping
+    @GetMapping("/books")
     public String welcome(){
         return "Welcome";
     }
+    @PostMapping("/books")
+    public String addBooks(){
+        return "Buku berhasil ditambahkan";
+    }
+    @PutMapping("/books")
+    public String editBooks(){
+        return "Buku berhasil diedit";
+    }
+    @DeleteMapping("/books")
+    public String deleteBooks(){
+        return "Buku berhasil dihapus";
+    }
+
+
 }
