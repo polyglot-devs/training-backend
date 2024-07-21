51dd01c5cd4f287657b33fd71efac6e5df9143b7
asyraf07 <asyraf.sr07@gmail.com>
2024-07-21 22:02:04 +0700

    refactor controller books

--- src/main/java/com/polyglot/training/controller/BooksController.java
+++ src/main/java/com/polyglot/training/controller/BooksController.java
@@ -3,23 +3,25 @@ package com.polyglot.training.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class ControllerHome {
+public class BooksController {
+
     @GetMapping("/books")
     public String welcome(){
         return "Welcome";
     }
+
     @PostMapping("/books")
     public String addBooks(){
         return "Buku berhasil ditambahkan";
     }
+
     @PutMapping("/books")
     public String editBooks(){
         return "Buku berhasil diedit";
     }
+
     @DeleteMapping("/books")
     public String deleteBooks(){
         return "Buku berhasil dihapus";
     }
-
-
 }
