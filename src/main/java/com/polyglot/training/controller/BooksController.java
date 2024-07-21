ad9241e7a6802b8c7a5d27e4f8a60d90483a3d2f
ranisilvani <ranisyach@gmail.com>
2024-07-21 22:56:31 +0700

    Merubah respons status

--- src/main/java/com/polyglot/training/controller/BooksController.java
+++ src/main/java/com/polyglot/training/controller/BooksController.java
@@ -1,27 +1,34 @@
 package com.polyglot.training.controller;
 
+import com.polyglot.training.dto.BooksDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BooksController {
 
     @GetMapping("/books")
-    public String welcome(){
-        return "Welcome";
+    public ResponseEntity<BooksDTO> welcome(){
+        BooksDTO data = new BooksDTO();
+        data.setId(1);
+        data.setAuthor("Tere Liye");
+        data.setTitle("Bulan");
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
     @PostMapping("/books")
-    public String addBooks(){
-        return "Buku berhasil ditambahkan";
+    public ResponseEntity<String> addBooks(){
+        return new ResponseEntity<>("Buku berhasil ditambahkan", HttpStatus.CREATED);
     }
 
     @PutMapping("/books")
-    public String editBooks(){
-        return "Buku berhasil diedit";
+    public ResponseEntity<String> editBooks(){
+        return new ResponseEntity<>("Title harus diisi", HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/books")
-    public String deleteBooks(){
-        return "Buku berhasil dihapus";
+    public ResponseEntity<String> deleteBooks(){
+        return new ResponseEntity<>("Buku tidak ditemukan", HttpStatus.NOT_FOUND);
     }
 }
---
+++ src/main/java/com/polyglot/training/dto/BooksDTO.java
@@ -0,0 +1,10 @@
+package com.polyglot.training.dto;
+
+import lombok.Data;
+
+@Data
+public class BooksDTO {
+    private Integer id;
+    private String title;
+    private String author;
+}
