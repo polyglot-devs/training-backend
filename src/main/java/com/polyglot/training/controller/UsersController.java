04b0c5ba10a37d515f2e26131904c5a14d5a4020
ranisilvani <ranisyach@gmail.com>
2024-08-02 15:34:11 +0700

    login

--- src/main/java/com/polyglot/training/controller/UsersController.java
+++ src/main/java/com/polyglot/training/controller/UsersController.java
@@ -17,8 +17,27 @@ public class UsersController {
             UsersDTO.builder().id(1).name("rani").password("asdf").build()
     );
 
+    @GetMapping("/login")
+    public ResponseEntity<AlertDTO> getLogin(@RequestParam String username, @RequestParam String password) {
+        HttpStatus status = HttpStatus.OK;
+        AlertDTO data = new AlertDTO();
+        for (int i = 0; i < dataUser.size(); i++) {
+            if (dataUser.get(i).getName().equals(username) && dataUser.get(i).getPassword().equals(password)) {
+                data.setMessage("Login Berhasil");
+                data.setStatus(true);
+                status = HttpStatus.OK;
+                break;
+            } else {
+                data.setMessage("Login Gagal");
+                data.setStatus(false);
+                status = HttpStatus.BAD_REQUEST;
+            }
+        }
+        return new ResponseEntity<>(data, status);
+    }
+
     @GetMapping
-    public ResponseEntity<UsersDTO> getUsers(){
+    public ResponseEntity<UsersDTO> getUsers() {
         UsersDTO data = new UsersDTO();
         data.setId(1);
         data.setName("Risa");
@@ -27,7 +46,7 @@ public class UsersController {
     }
 
     @PostMapping
-    public ResponseEntity<AlertDTO> addUsers(){
+    public ResponseEntity<AlertDTO> addUsers() {
         AlertDTO data = new AlertDTO();
         data.setMessage("Data pengguna berhasil ditambahkan");
         data.setStatus(true);
@@ -35,7 +54,7 @@ public class UsersController {
     }
 
     @PutMapping
-    public ResponseEntity<AlertDTO> editUsers(){
+    public ResponseEntity<AlertDTO> editUsers() {
         AlertDTO data = new AlertDTO();
         data.setMessage("Data pengguna berhasil diedit");
         data.setStatus(true);
@@ -43,7 +62,7 @@ public class UsersController {
     }
 
     @DeleteMapping
-    public ResponseEntity<AlertDTO> deleteUsers(){
+    public ResponseEntity<AlertDTO> deleteUsers() {
         AlertDTO data = new AlertDTO();
         data.setMessage("Data yang ingin dihapus tidak ditemukan");
         data.setStatus(false);
